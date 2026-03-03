f2ba951bcbc66536d05a8e689b98825b8f4c1142
nikodp14 <nicolas_chec@hotmail.com>
2026-03-03 16:06:52 -0300

    Se corrige server.js

--- server.js
+++ server.js
@@ -378,8 +378,8 @@ app.get("*", (req, res) => {
   renderWithSidebar(res, path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor corriendo en puerto", PORT);
 });
